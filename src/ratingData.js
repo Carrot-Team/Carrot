a267ab8ee430418766546ee6c3c8c2ecac15bac0
Peter <s1acker90@mai.ru>
2020-12-09 21:27:42 +0300

    Моковые данные
    Добавил фотки юзеров и призов, создал 2 файлы с датой, на юзеров и на рейтинги

---
+++ src/ratingData.js
@@ -0,0 +1,22 @@
+export const userData = [
+    {
+        id: '1',
+        name: 'Продажи',
+        prizes: [
+            { img: '../assets/prizes/1.jpg' },
+            { img: '../assets/prizes/2.jpg' },
+            { img: '../assets/prizes/3.jpg' },
+        ],
+        deadline: '12.01.2021',
+    },
+    {
+        id: '2',
+        name: 'Магазины',
+        prizes: [
+            { img: '../assets/prizes/1.jpg' },
+            { img: '../assets/prizes/2.jpg' },
+            { img: '../assets/prizes/3.jpg' },
+        ],
+        deadline: '12.03.2021',
+    },
+]
